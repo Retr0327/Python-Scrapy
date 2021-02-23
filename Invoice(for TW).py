75cced2261bdb5f40c95c9b28e98095204262ce9
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2021-02-23 15:01:47 +0800

    Update Invoice(for TW).py

--- Invoice(for TW).py
+++ Invoice(for TW).py
@@ -1,13 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-res=requests.get('https://invoices.com.tw/1112.html')
+month=str(input("請輸入月份："))
+res=requests.get('https://invoices.com.tw/'+month+'.html')
 res.encoding='utf-8'
 bsObj=BeautifulSoup(res.text, "html.parser")
 title=bsObj.find('title').text
 date=bsObj.findAll("td", {"colspan":"3"})
 win=bsObj.findAll('tr', {'class':"tr"})
-print(title.split(' ')[0], '11、12月發票')
+print(title.split(' ')[0][:4], f'{month[:2]}、{month[2:]}月發票')
 print("領獎時間：", ' '.join(date[2].text.split(' ')[2:5]))
 special=win[0].text.split('\n')[2]
 jury=win[1].text.split('\n')[1] 
@@ -19,34 +21,35 @@ print("3. 頭獎：", ' / '.join(jackpot))
 print("4. 增開：", addit.replace("、", ' / '), '\n')
 
 while True:
-    a=str(input("請輸入發票數字碼："))
-    print(a)
-    if a =="e" or a=="E":
+    target=str(input("請輸入發票數字碼："))
+    target=re.sub(r'\s', '', target)
+    print(target)
+    if target =="e" or target=="E":
         break
-    if a == special:
+    if target == special:
         print("中特別獎1,000萬元")
         continue
-    elif a == jury:
+    elif target == jury:
         print("中特獎2,00萬元")
         continue
-    elif a in jackpot:
+    elif target in jackpot:
         print("中頭獎20萬元")
         continue
-    elif a[1:] in list(map(lambda i: i[1:], jackpot)):
+    elif target[1:] in list(map(lambda i: i[1:], jackpot)):
         print("中二獎4萬元")
         continue
-    elif a[2:] in list(map(lambda i: i[2:], jackpot)):
+    elif target[2:] in list(map(lambda i: i[2:], jackpot)):
         print("中三獎1萬元")
         continue
-    elif a[3:] in list(map(lambda i: i[3:], jackpot)):
+    elif target[3:] in list(map(lambda i: i[3:], jackpot)):
         print("中四獎4千元")
         continue      
-    elif a[4:] in list(map(lambda i: i[4:], jackpot)):
+    elif target[4:] in list(map(lambda i: i[4:], jackpot)):
         print("中五獎1千元")
         continue
-    elif a[5:] in list(map(lambda i: i[5:], jackpot)):
+    elif target[5:] in list(map(lambda i: i[5:], jackpot)):
         print("中六獎200元")
         continue
-    elif a[-3:] in addit.split('、'):
+    elif target[-3:] in addit.split('、'):
         print("中增開六獎200元")
         continue
